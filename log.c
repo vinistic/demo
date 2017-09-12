78647e9f27cb965da871d0d146bf73a0cb46d06b
Vini Sawjiani <svini30@gmail.com>
2017-09-12 17:46:05 +0530

    Update log

--- log.c
+++ log.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
 #include <math.h>
-
-int main ()
+int log(x)
 {
-   double x, ret;
-   x = 2.7;
-
-   /* finding log(2.7) */
-   ret = log(x);
-   printf("log(%lf) = %lf", x, ret);
-   
-   return(0);
+  return log(x);
 }
